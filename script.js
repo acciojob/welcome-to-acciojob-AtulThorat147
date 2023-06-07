303273c3ad06d36bad79492005f5fe71688c849a
ᴀᴛᴜʟ × ᴛʜᴏʀᴀᴛ <125597552+AtulThorat147@users.noreply.github.com>
2023-06-07 18:28:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- the entire body must be written by student -->
+    <script type="text/javascript" src="./script.js"></script>
+    <script>
+        // Add your JavaScript code here
+        var welcomeMessage = document.createElement("div");
+        welcomeMessage.innerText = "Welcome to Accio Jobs!";
+        document.body.appendChild(welcomeMessage);
+    </script>
+</body>
+</html>
